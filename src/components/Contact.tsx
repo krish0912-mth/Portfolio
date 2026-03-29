988bb5acb52b9814fafcc38719215bf7ec824e49
krish0912-mth <krishshrivastava55@gmail.com>
2026-03-29 13:35:33 +0530

    first commit

---
+++ src/components/Contact.tsx
@@ -0,0 +1,69 @@
+import { MdArrowOutward, MdCopyright } from "react-icons/md";
+import "./styles/Contact.css";
+
+const Contact = () => {
+  return (
+    <div className="contact-section section-container" id="contact">
+      <div className="contact-container">
+        <h3>Contact</h3>
+        <div className="contact-flex">
+          <div className="contact-box">
+            <h4>Email</h4>
+            <p>
+              <a href="krishshrivastava55@gmail.com," data-cursor="disable">
+                krishshrivastava55@gmail.com
+              </a>
+            </p>
+            <h4>Education</h4>
+            <p>B.Tech In Computer Science</p>
+          </div>
+          <div className="contact-box">
+            <h4>Social</h4>
+            <a
+              href="https://github.com/krish0912-mth"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Github <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/krish-shrivastav-682589298?utm_source=share_via&utm_content=profile&utm_medium=member_android"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Linkedin <MdArrowOutward />
+            </a>
+            <a
+              href="https://x.com/Krishsh9631000"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Twitter <MdArrowOutward />
+            </a>
+            <a
+              href="https://www.instagram.com/krish_srivastav_09/"
+              target="_blank"
+              data-cursor="disable"
+              className="contact-social"
+            >
+              Instagram <MdArrowOutward />
+            </a>
+          </div>
+          <div className="contact-box">
+            <h2>
+              Designed and Developed <br /> by <span>Krish Shrivastava</span>
+            </h2>
+            <h5>
+              <MdCopyright /> 2026
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
